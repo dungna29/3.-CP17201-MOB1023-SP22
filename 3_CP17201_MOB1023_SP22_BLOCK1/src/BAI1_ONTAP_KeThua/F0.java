74f193f581fdcc38d410fd7574879a12df0fee7f
Dungna89 <dungna29@fpt.edu.vn>
2022-01-04 14:05:46 +0700

    1

---
+++ 3_CP17201_MOB1023_SP22_BLOCK1/src/BAI1_ONTAP_KeThua/F0.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BAI1_ONTAP_KeThua;
+
+/**
+ *
+ * @author Dungna89
+ */
+public class F0 {//Coi thế hệ đầu tiên
+  private String variable1;
+  private String variable2;
+  private String variable3;
+
+  public F0() {
+  }
+
+  public F0(String variable1, String variable2, String variable3) {
+    this.variable1 = variable1;
+    this.variable2 = variable2;
+    this.variable3 = variable3;
+  }
+
+  public String getVariable1() {
+    return variable1;
+  }
+
+  public void setVariable1(String variable1) {
+    this.variable1 = variable1;
+  }
+
+  public String getVariable2() {
+    return variable2;
+  }
+
+  public void setVariable2(String variable2) {
+    this.variable2 = variable2;
+  }
+
+  public String getVariable3() {
+    return variable3;
+  }
+
+  public void setVariable3(String variable3) {
+    this.variable3 = variable3;
+  }
+  
+  public void method1() {
+
+  }
+
+  private void method2() {
+
+  }
+
+  public void method3() {
+
+  }
+}
---
+++ 3_CP17201_MOB1023_SP22_BLOCK1/src/BAI1_ONTAP_KeThua/F1.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BAI1_ONTAP_KeThua;
+
+/**
+ *
+ * @author Dungna89
+ */
+public class F1 extends F0 {
+
+  private String variable4;
+  private String variable5;
+
+  public F1() {
+  }
+
+  public F1(String variable4A, String variable5, String variable1, String variable2, String variable3) {
+    super(variable1, variable2, variable3);
+    variable4 = variable4A;
+    this.variable5 = variable5;
+    //this: Tham chiếu đến thuộc tính và phương thức của lớp hiện tại
+    //super:Tham chiếu đến thuộc tính và phương thức của lớp cha
+  }
+
+  public String getVariable4() {
+    return variable4;
+  }
+
+  public void setVariable4(String variable4) {
+    this.variable4 = variable4;
+  }
+
+  public String getVariable5() {
+    return variable5;
+  }
+
+  public void setVariable5(String variable5) {
+    this.variable5 = variable5;
+  }
+
+  @Override
+  public void method3() {
+    //Code lại ở đây
+  }
+
+  @Override
+  public void method1() {
+    super.method1(); //To change body of generated methods, choose Tools | Templates.
+  }
+  
+  
+}
