30b42dec76c8c0d720f584364ead0afc0666d782
dungna29 <dungna29@localhost>
2022-02-18 13:14:55 +0700

    Dungna 2022

--- 3_CP17201_MOB1023_SP22_BLOCK1/src/BAI7_Thread/S1_Thread.java
+++ 3_CP17201_MOB1023_SP22_BLOCK1/src/BAI7_Thread/S1_Thread.java
@@ -11,7 +11,7 @@ package BAI7_Thread;
  */
 public class S1_Thread {
       /*
-    Dungnz29
+    Dungnz29-Testcommit
         ❑Multitasking: Là khả năng chạy đồng thời nhiều
         chương trình cùng một lúc trên hệ điều hành.
         ❖Internet Explorer
